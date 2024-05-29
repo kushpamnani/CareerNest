f0e61df15cec430fbaa4cff01813fc05166a5204
Kush Pamnani <137836006+kushpamnani@users.noreply.github.com>
2024-05-29 09:28:49 -0400

    comments

--- final/frontend/src/index.js
+++ final/frontend/src/index.js
@@ -1,10 +1,14 @@
+/*
+Author: Kush Pamnani
+*/
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
+//rendering the application
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
